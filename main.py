0db7a5a18c903cb5b771dd6f3a6fe550f5f2b4c5
Nemanja Markovic <necamark@gmail.com>
2024-05-27 15:20:45 +0200

    "flipping cards functionality implemented"

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import csv
 from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
-window = Tk()
-window.title("Flashy")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 words = []
 with open("data/french_words.csv") as file:
@@ -16,14 +14,31 @@ with open("data/french_words.csv") as file:
 
 
 def next_card():
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=choice(words)["french"])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = choice(words)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["french"], fill="black")
+    canvas.itemconfig(card_background, image=front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["english"], fill="white")
+    canvas.itemconfig(card_background, image=back_img)
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(height=526, width=800)
 front_img = PhotoImage(file="images/card_front.png")
-# back_img = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=front_img)
+back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
